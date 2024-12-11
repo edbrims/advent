a926b91836ba1a934df64452c316666be6464d6f
Ed Brims <42276776+edbrims@users.noreply.github.com>
2024-12-11 09:37:56 +0000

    Day 11 (stones)

--- 2024/day_09.py
+++ 2024/day_09.py
@@ -7,7 +7,7 @@ example_input = '''
 
 lines = get_input(use_real, example_input, __file__)
 
-class Block:
+class File:
     def __init__(self, id, length, start):
         self.id = id
         self.length = length
@@ -23,82 +23,81 @@ class Block:
         return f'({self.id} in {self.start}-{self.end()})'
 
 
-def read_blocks(line):
-    blocks = []
+def read_files_and_gaps(instructions):
+    files = []
     gaps = []
     position = 0
     id = 0
-    for i in range(len(line) // 2 + 1):
-        length = int(line[2 * i])
+    for i in range(len(instructions) // 2 + 1):
+        length = int(instructions[2 * i])
         if length > 0:
-            blocks.append(Block(id, length, position))
+            files.append(File(id, length, position))
         position += length
 
-        if (2 * i + 1 >= len(line)):
+        if (2 * i + 1 >= len(instructions)):
             break
-        gap_length = int(line[2 * i + 1])
+        gap_length = int(instructions[2 * i + 1])
         if gap_length > 0:
-            gaps.append(Block(None, gap_length, position))
+            gaps.append(File(None, gap_length, position))
         position += gap_length
         id += 1
-    return blocks, gaps
+    return files, gaps
 
-def defrag_by_block(blocks, gaps):
-    while gaps and (gaps[0].start < blocks[-1].end()):
+def defrag_by_block(instructions):
+    files, gaps = read_files_and_gaps(instructions)
+    while gaps and (gaps[0].start < files[-1].end()):
         first_gap = gaps[0]
-        last_block = blocks[-1]
-        if last_block.length == first_gap.length:
+        last_file = files[-1]
+        if last_file.length == first_gap.length:
             # Perfect fit!
             gaps.pop(0)
-            blocks.pop()
-            blocks.insert(0, Block(last_block.id, last_block.length, first_gap.start))
-        elif last_block.length < first_gap.length:
-            # Small block in a big gap. Shrink the gap.
-            blocks.pop()
-            blocks.insert(0, Block(last_block.id, last_block.length, first_gap.start))
-            gaps[0].start += last_block.length
-            gaps[0].length -= last_block.length
+            files.pop()
+            files.insert(0, File(last_file.id, last_file.length, first_gap.start))
+        elif last_file.length < first_gap.length:
+            # Small file in a big gap. Shrink the gap.
+            files.pop()
+            files.insert(0, File(last_file.id, last_file.length, first_gap.start))
+            gaps[0].start += last_file.length
+            gaps[0].length -= last_file.length
         else:
-            # Big block in a small gap. Chop it up to fill the gap.
-            blocks.insert(0, Block(last_block.id, first_gap.length, first_gap.start))
+            # Big file in a small gap. Chop it up to fill the gap.
+            files.insert(0, File(last_file.id, first_gap.length, first_gap.start))
             gaps.pop(0)
-            blocks[-1].length -= first_gap.length
-    return blocks
+            files[-1].length -= first_gap.length
+    return files
 
-def index_of_first_gap_big_enough(block, gaps):
+def index_of_first_gap_big_enough(file, gaps):
     for i in range(len(gaps)):
         gap = gaps[i]
-        if gap.length >= block.length:
+        if gap.length >= file.length:
             return i
     return None
 
-def defrag_by_file(blocks, gaps):
-    for i in reversed(range(len(blocks))):
-        block = blocks[i]
+def defrag_by_file(instructions):
+    files, gaps = read_files_and_gaps(instructions)
+    for i in reversed(range(len(files))):
+        file = files[i]
 
-        gap_index = index_of_first_gap_big_enough(block, gaps)
-        if gap_index is not None and gaps[gap_index].start < block.start:
+        gap_index = index_of_first_gap_big_enough(file, gaps)
+        if gap_index is not None and gaps[gap_index].start < file.start:
             gap_to_fill = gaps[gap_index]
-            blocks[i].start = gap_to_fill.start
-            if block.length == gap_to_fill.length:
+            files[i].start = gap_to_fill.start
+            if file.length == gap_to_fill.length:
                 # Perfect fit - delete the gap.
                 gaps.pop(gap_index)
             else:
                 # We've only filled part of the gap
-                gaps[gap_index].start += block.length
-                gaps[gap_index].length -= block.length
+                gaps[gap_index].start += file.length
+                gaps[gap_index].length -= file.length
         else:
             # Can't be moved
             pass
 
-    return blocks
+    return files
 
-def checksum_sum(blocks):
-    return sum([b.checksum() for b in blocks])
+def checksum_sum(files):
+    return sum([b.checksum() for b in files])
 
-blocks, gaps = read_blocks(lines[0])
-print(f'Part 1: {checksum_sum(defrag_by_block(blocks, gaps))}') # 6432869891895
-
-blocks, gaps = read_blocks(lines[0])
-print(f'Part 2: {checksum_sum(defrag_by_file(blocks, gaps))}') # 6467290479134
+print(f'Part 1: {checksum_sum(defrag_by_block(lines[0]))}') # 6432869891895
+print(f'Part 2: {checksum_sum(defrag_by_file(lines[0]))}') # 6467290479134
 # First guess 8664287909542 was too high because I didn't insist on moving left.
