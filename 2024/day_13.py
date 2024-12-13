d68e8cd7ae428e3fbf0cac728adf0753d953761e
edbrims <edbrims@gmail.com>
2024-12-13 01:06:17 +0000

    Can I commit from my Chromebook?

---
+++ 2024/day_13.py
@@ -0,0 +1,13 @@
+from input_loader import get_input
+
+use_real = False
+example_input = '''
+Example
+'''
+
+lines = get_input(use_real, example_input, __file__)
+
+#
+
+print(f"Part 1: {0}")
+print(f"Part 2: {0}")
